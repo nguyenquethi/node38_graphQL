9166963278e958e8e7ed2af0d842e932e48be841
Thi <nguyenquethi186@gmail.com>
2024-02-05 17:56:27 -0500

    A1

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 mfksfm
 npm i express nodemon prisma @prisma/client mysql2 dotenv graphql express-graphql
+
+rebase and merge? Rebase: connect branch after master branch/ merge: merge branch and master branch together follow time order.
+
+Merge commits to one commit: git log --oneline -> git rebase -i HEAD ~ {number of commits}
+
+revert commit: git log --oneline(to retrieve commit id) -> git reset --hard <commit id> -> git push origin <branch name> --force
--- index.js
+++ index.js
@@ -1,6 +1,40 @@
 import express from 'express';
+import { buildSchema } from 'graphql'; 
+import { graphqlHTTP } from 'express-graphql';
 
+const rootSchema = buildSchema(`
+    type user {
+        id: Int
+        hoTen: String
+    }
+    type rootMutation {
+        createUser: String
+    }
+    type rootQuery{
+        getUser(id: String, hoTen: String): user
+    }
+    schema {
+        query: rootQuery
+        mutation: rootMutation
+    }
+`);
+const resolver = {
+    getUser: ({id, hoTen}) => {
+        return {
+            id: Number(id),
+            hoTen: hoTen
+        }
+    },
+    createUser: () => {
+        return "createUser"
+    }
+}
 const app = express();
+app.use("/graph", graphqlHTTP({
+    schema: rootSchema,
+    rootValue: resolver,
+    graphiql: true
+}))
 
 app.listen(8081, () => {
     console.log("BE start");
